6ad20cb0f75fb1430525091defd933b9c4648b14
graywatson <graywatson@565b383f-4439-42de-b011-d3c6644513c9>
2012-04-03 16:03:06 +0000

    cp

--- src/test/java/com/j256/simplejmx/client/MainTest.java
+++ src/test/java/com/j256/simplejmx/client/MainTest.java
@@ -1,42 +1,70 @@
 package com.j256.simplejmx.client;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.PrintStream;
 
 import javax.management.JMException;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.j256.simplejmx.server.JmxServer;
 
 public class MainTest {
 
+	private static PrintStream outSave = System.out;
+	private static PrintStream errSave = System.err;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		PrintStream ignored = new PrintStream(new File("target/ignored"));
+		// we do this to stop spitting out error output
+		System.setOut(ignored);
+		System.setErr(ignored);
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		System.setOut(outSave);
+		System.setErr(errSave);
+	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testNoArgs() throws Exception {
+		System.setErr(new PrintStream(new File("target/ignored")));
 		new Main().doMain(new String[0], true);
 	}
 
 	@Test
 	public void testStatic() throws Exception {
+		System.setErr(new PrintStream(new File("target/ignored")));
+		System.setOut(new PrintStream(new File("target/ignored")));
 		Main.main(new String[0]);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidHostNamePortFormat() throws Exception {
+		System.setErr(new PrintStream(new File("target/ignored")));
 		new Main().doMain(new String[] { "foo" }, true);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidPortNumber() throws Exception {
+		System.setErr(new PrintStream(new File("target/ignored")));
 		new Main().doMain(new String[] { "localhost:notport" }, true);
 	}
 
 	@Test(expected = JMException.class)
 	public void testConnectToNoServer() throws Exception {
+		System.setErr(new PrintStream(new File("target/ignored")));
 		new Main().doMain(new String[] { "localhost:18080" }, true);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testTooManyArgs() throws Exception {
+		System.setErr(new PrintStream(new File("target/ignored")));
 		new Main().doMain(new String[] { "localhost:18080", "2", "3" }, true);
 	}
 
