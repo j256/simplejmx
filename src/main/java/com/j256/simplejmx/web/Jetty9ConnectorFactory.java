4f5da22a89a4e829354968cf9cf2d7ec03ed1e35
Gray Watson <gray.github@mailnull.com>
2014-06-04 11:03:16 -0400

    Switched jetty9 hack to use reflection.

--- src/main/java/com/j256/simplejmx/web/Jetty9ConnectorFactory.java
+++ src/main/java/com/j256/simplejmx/web/Jetty9ConnectorFactory.java
@@ -1,19 +1,27 @@
 package com.j256.simplejmx.web;
 
+import java.lang.reflect.Constructor;
+
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
 
 /**
- * Configuration of our Jetty {@link Connector} if using Jetty version 9.
+ * Configuration of our Jetty {@link Connector} if using Jetty version 9. We have to use reflection here because Jetty 9
+ * requires Java 7 which I'm not ready to require.
  * 
  * @author graywatson
  */
 public class Jetty9ConnectorFactory implements JettyConnectorFactory {
 
 	public Connector buildConnector(Server server, int serverPort) {
-		ServerConnector connector = new ServerConnector(server);
-		connector.setPort(serverPort);
-		return connector;
+		try {
+			Class<?> clazz = Class.forName("org.eclipse.jetty.server.ServerConnector");
+			Constructor<?> constructor = clazz.getConstructor(Server.class);
+			Connector connector = (Connector) constructor.newInstance(server);
+			connector.setPort(serverPort);
+			return connector;
+		} catch (Exception e) {
+			throw new RuntimeException("could not create ServerConnector with reflection", e);
+		}
 	}
 }
