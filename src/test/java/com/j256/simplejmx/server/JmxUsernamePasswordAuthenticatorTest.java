15c95e900489c4cda9715569f4b17b8525a10801
Gray Watson <gray.github@mailnull.com>
2017-03-20 13:13:36 -0400

    Revert "Too early."
    This reverts commit 1a78263c1effb3da40ecf7881169e4410ab7d316.

---
+++ src/test/java/com/j256/simplejmx/server/JmxUsernamePasswordAuthenticatorTest.java
@@ -0,0 +1,39 @@
+package com.j256.simplejmx.server;
+
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.j256.simplejmx.client.JmxClient;
+import com.j256.simplejmx.common.IoUtils;
+
+@Ignore("not ready for prime time yet")
+public class JmxUsernamePasswordAuthenticatorTest {
+
+	private static final int DEFAULT_PORT = 5256;
+
+	@Test
+	public void testAuthentication() throws Exception {
+		InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
+		JmxServer server = new JmxServer(serverAddress, DEFAULT_PORT);
+		JmxClient client = null;
+		try {
+			JmxUsernamePasswordAuthenticator authenticator = new JmxUsernamePasswordAuthenticator();
+			Map<String, String> authMap = new HashMap<String, String>();
+			String username = "hello";
+			String password = "there";
+			authMap.put(username, password);
+			authenticator.setAuthMap(authMap);
+			server.setAuthenticator(authenticator);
+			server.start();
+			Thread.sleep(10000000);
+			// client = new JmxClient(serverAddress, DEFAULT_PORT, username, password);
+		} finally {
+			IoUtils.closeQuietly(client);
+			IoUtils.closeQuietly(server);
+		}
+	}
+}
