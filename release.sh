415498a0733ff897387a8389d007a157fd17e17f
Gray Watson <gray.github@mailgw.com>
2026-04-21 19:13:14 -0400

    cp

--- release.sh
+++ release.sh
@@ -7,33 +7,6 @@ LIBRARY=`basename $(pwd)`
 LOCAL_DIR="$HOME/svn/local/$LIBRARY"
 
 #############################################################
-# check initial stuff
-
-bad=0
-
-git status | head -1 | fgrep main > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    /bin/echo "Should be on main branch."
-    git status | head -1
-    bad=1
-fi
-
-head -1 src/main/javadoc/doc-files/changelog.txt | fgrep '?' > /dev/null 2>&1
-if [ $? -ne 1 ]; then
-    /bin/echo "No question-marks (?) can be in the ChangeLog top line."
-    head -1 src/main/javadoc/doc-files/changelog.txt
-    bad=1
-fi
-
-cd $LOCAL_DIR
-git status | grep 'nothing to commit'
-if [ $? -ne 0 ]; then
-    /bin/echo "Files not checked-in"
-    git status
-    bad=1
-fi
-
-#############################################################
 
 release=$(grep version pom.xml | grep SNAPSHOT | head -1 | cut -f2 -d\> | cut -f1 -d\-)
 
@@ -46,26 +19,67 @@ if [ "$rel" != "" ]; then
 fi
 
 #############################################################
+# check initial stuff
+
+bad=0
+
+git status | head -1 | fgrep main > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    /bin/echo "ERROR: Should be on main branch."
+    git status | head -1
+    bad=1
+fi
+
+head -1 src/main/javadoc/doc-files/changelog.txt | fgrep '?' > /dev/null 2>&1
+if [ $? -ne 1 ]; then
+    /bin/echo "ERROR: No question-marks (?) can be in the ChangeLog top line."
+    head -1 src/main/javadoc/doc-files/changelog.txt
+    bad=1
+fi
+
+cd $LOCAL_DIR
+git status | grep 'nothing to commit'
+if [ $? -ne 0 ]; then
+    /bin/echo "ERROR: Files not checked-in"
+    git status
+    bad=1
+fi
+
+echo "running javadoc tests"
+mvn -B -q javadoc:javadoc
+if [ $? -ne 0 ]; then
+    /bin/echo "ERROR: javadoc errors exist"
+    bad=1
+fi
+
+cd $LOCAL_DIR
+mvn javadoc:javadoc | grep WARNING
+if [ $? -eq 0 ]; then
+    /bin/echo "ERROR: javadoc warnings exist"
+    bad=1
+fi
+
+#############################################################
 # check docs:
 
 cd $LOCAL_DIR
 ver=$(head -1 src/main/javadoc/doc-files/changelog.txt | cut -f1 -d:)
 if [ "$release" != "$ver" ]; then
-    /bin/echo "Change log top line version seems wrong:"
+    /bin/echo "ERROR: Change log top line version seems wrong:"
     head -1 src/main/javadoc/doc-files/changelog.txt
     bad=1
 fi
 
 grep -q $release README.md
 if [ $? != 0 ]; then
-    /bin/echo "Could not find $release in README.md"
+    /bin/echo "ERROR: Could not find $release in README.md"
     bad=1
 fi
 
 if [ -r "src/main/doc/$LIBRARY.texi" ]; then
     ver=$(grep "^@set ${LIBRARY}_version" src/main/doc/$LIBRARY.texi | cut -f3 -d' ')
     if [ "$release" != "$ver" ]; then
-	/bin/echo "$LIBRARY.texi version seems wrong:"
+	/bin/echo "ERROR: $LIBRARY.texi version seems wrong:"
 	grep "^@set ${LIBRARY}_version" src/main/doc/$LIBRARY.texi
 	bad=1
     fi
@@ -74,7 +88,7 @@ fi
 if [ -r "src/main/javadoc/doc-files/$LIBRARY.html" ]; then
     grep "Version $release" src/main/javadoc/doc-files/$LIBRARY.html > /dev/null
     if [ $? -ne 0 ]; then
-	/bin/echo "javadoc doc-files $LIBRARY.html version seems wrong:"
+	/bin/echo "ERROR: javadoc doc-files $LIBRARY.html version seems wrong:"
 	grep "Version " src/main/javadoc/doc-files/$LIBRARY.html
 	bad=1
     fi
